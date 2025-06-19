098766114c67fddb4b76d9959ca2d3f3b5fb6106
Bethelihem Wondimneh <wondimnehbethelihem@gmail.com>
2025-06-19 15:45:36 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// const hamburger = document.getElementById('hamburger');
+// const navMenu = document.getElementById('navmenu');
+// hamburger.addEventListener('click', () => {
+//     navMenu.classList.toggle('active');
+// });
+
+
+const myBtn = document.getElementById("btn1");
+        myBtn.addEventListener("click", function() {
+          window.location.href = "http://127.0.0.1:5500/about_me.html";
+        });
+
+
+        const hamburger = document.getElementById('hamburger');
+        const navMenu = document.getElementById('navmenu');
+        hamburger.addEventListener('click', () => {
+            navMenu.classList.toggle('active');
+        });
+        
